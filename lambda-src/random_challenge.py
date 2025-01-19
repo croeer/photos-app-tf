d2ecb2d02405efcadcf5a7e7a37520d146f299bc
croeer <christian@roeer.info>
2025-01-19 21:34:03 +0100

    remove Access-Control-Allow-Origin header from response in random_challenge.py

--- lambda-src/random_challenge.py
+++ lambda-src/random_challenge.py
@@ -61,7 +61,6 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
         },
         "body": json.dumps({"challenge": challenge}),
     }
